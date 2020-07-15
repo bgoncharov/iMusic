1a1c629c4f437e23853c27d9009bbf632cff65a1
Boris Goncharov <bora4bora@gmail.com>
2020-07-15 15:47:03 -0700

    init

---
+++ iMusic/MainTabBarController.swift
@@ -0,0 +1,19 @@
+//
+//  MainTabBarController.swift
+//  iMusic
+//
+//  Created by Boris Goncharov on 7/15/20.
+//  Copyright © 2020 Boris Goncharov. All rights reserved.
+//
+
+import UIKit
+
+
+class MainTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .gray
+    }
+}
---
+++ iMusic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iMusic
+//
+//  Created by Boris Goncharov on 7/15/20.
+//  Copyright © 2020 Boris Goncharov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
