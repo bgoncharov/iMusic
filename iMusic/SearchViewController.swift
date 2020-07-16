f6ec7acc6d0fe5ab7ee619b9ca0486279a99039a
Boris Goncharov <bora4bora@gmail.com>
2020-07-16 14:56:47 -0700

    Alamofire + Itunes Store API

--- iMusic/SearchViewController.swift
+++ iMusic/SearchViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Alamofire
 
 struct TrackModel {
     var trackName: String
@@ -53,6 +54,18 @@ class SearchViewController: UITableViewController {
 extension SearchViewController: UISearchBarDelegate {
     
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        print(searchText)
+//        print(searchText)
+        let url = "https://itunes.apple.com/search?term=\(searchText)"
+        
+        AF.request(url).responseData { (dataResponse) in
+            
+            if let error = dataResponse.error {
+                print("Error: \(error.localizedDescription)")
+            }
+            
+            guard let data = dataResponse.data else { return }
+            let someString = String(data: data, encoding: .utf8)
+            print(someString ?? "")
+        }
     }
 }
