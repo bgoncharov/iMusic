be5b76eb040d7943dbfdd6cc43b658146aa4b2b1
Boris Goncharov <bora4bora@gmail.com>
2020-07-16 13:25:42 -0700

    Tracks model

--- iMusic/SearchViewController.swift
+++ iMusic/SearchViewController.swift
@@ -10,9 +10,31 @@ import UIKit
 
 class SearchViewController: UITableViewController {
     
+    struct TrackModel {
+        var trackName: String
+        var artistName: String
+    }
+    
+    let tracks = [TrackModel(trackName: "TNT", artistName: "AC/DC"), TrackModel(trackName: "Yesterday", artistName: "Beatles")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .white
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cellId")
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tracks.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath)
+        let track = tracks[indexPath.row]
+        cell.textLabel?.text = "\(track.trackName)\n\(track.artistName)"
+        cell.textLabel?.numberOfLines = 2
+        cell.imageView?.image = #imageLiteral(resourceName: "cover_domo_2")
+        return cell
     }
 }
