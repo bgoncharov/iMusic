b12284005cfc7660661f2e4ae23436537ca2d8b9
Boris Goncharov <bora4bora@gmail.com>
2020-07-20 12:32:52 -0700

    Track Detail xib

--- iMusic/Search/TrackDetailView/TrackDetailView.swift
+++ iMusic/Search/TrackDetailView/TrackDetailView.swift
@@ -10,10 +10,39 @@ import UIKit
 
 class TrackDetailView: UIView {
     
+    @IBOutlet weak var trackImageView: UIImageView!
+    @IBOutlet weak var currentTimeSlider: UISlider!
+    @IBOutlet weak var currentTimeLabel: UILabel!
+    @IBOutlet weak var durationTimeLabel: UILabel!
+    @IBOutlet weak var trackTitleLabel: UILabel!
+    @IBOutlet weak var authorTitleLabel: UILabel!
+    @IBOutlet weak var playPauseButton: UIButton!
+    @IBOutlet weak var volumeSlider: UISlider!
+    
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         
         backgroundColor = .blue
+    }
+    
+    @IBAction func dragDownButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func handleCurrentTimerSlider(_ sender: Any) {
+    }
+    
+    @IBAction func handleVolumeSlider(_ sender: Any) {
+    }
+    
+    @IBAction func previousTrack(_ sender: Any) {
+    }
+    
+    @IBAction func nextTrack(_ sender: Any) {
+    }
+    
+    @IBAction func playPauseAction(_ sender: Any) {
     }
     
 }
